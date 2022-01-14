644a51cb05f3582069f52d80e022f394500a0251
Sophie Kim <sophiekim@Sophies-MBP.hub>
2022-01-14 13:22:34 +1300

    Car Item Component

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -18,13 +18,22 @@ const styles = StyleSheet.create({
       subTitle: {
         fontSize: 16,
         color: '#5c5e62',
-      }
-      ,
+      },
+      subTitleCTA: {
+        fontSize: 16,
+        color: '#5c5e62',
+        textDecorationLine: 'underline'
+      },
       image: {
         width: '100%',
         height: '100%',
         resizeMode: 'cover',
         position: 'absolute',
+      },
+      buttonsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '100%',
       }
 });
 
